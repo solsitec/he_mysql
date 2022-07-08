53441cc6bdb8db1f800b6c57a9807846de8286b5
Luis Manuel Torres Carpio <32250454+luistorrescarpio@users.noreply.github.com>
2022-07-08 10:17:08 -0500

    Update index.php

--- he_mysql@1.2/example/index.php
+++ he_mysql@1.2/example/index.php
@@ -1,12 +1,16 @@
-<? require("../heasy_mysql.php");
+<? require("../heasy_mysql.php"); // Incluir en nuestro proyecto el Script
 
 // SELECT
 $r = query("SELECT * FROM persona WHERE id_persona=4");
+res($r); //Mostrar registro
 
 // UPDATE
 $r = query("UPDATE persona SET nick='Maria2022' WHERE id_persona=4 ");
+res($r); // Mostrar confirmación
+
 // DELETE
 $r = query("DELETE FROM persona WHERE id_persona=3");
+res($r); // Mostrar confirmación
 
 // INSERT // Nota: id_persona: Return ID
 $id = query("INSERT INTO persona (nombre, edad, nick)VALUES('Julio',30,'Cesar2021');");
@@ -18,4 +22,4 @@ if(isset($id["error"])){
 
 // SELECT ALL
 $r = query("SELECT * FROM persona");
-res($r);
+res($r); //Mostrar todos los datos
